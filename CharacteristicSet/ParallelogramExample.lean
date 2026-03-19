7658d43a8d6495abb8515673d5161668284f1308
tsuki8 <3118181069@qq.com>
2026-03-19 19:01:21 +0800

    add sage code

--- CharacteristicSet/ParallelogramExample.lean
+++ CharacteristicSet/ParallelogramExample.lean
@@ -1,7 +1,8 @@
+import MonomialOrderedPolynomial
 import CharacteristicSet.CharacteristicSet
 import CharacteristicSet.WeakAscendingSet
 
-open MvPolynomial WeakAscendingSet
+open MvPolynomial WeakAscendingSet MonomialOrder
 
 scoped[MvPolynomial] notation:9000 R "[" σ "]" => MvPolynomial σ R
 
@@ -17,13 +18,15 @@ def p₆ : ℚ[Fin 8] := X 4 + X 7 - (X 5 + X 6)
 
 def l : List ℚ[Fin 8] := [p₁, p₂, p₃, p₄]
 
+
 def lCS := [p₅, p₆]
 lemma lCS_non_zero : ∀ p ∈ lCS, p ≠ 0 := fun p hp ↦ by
   simp only [lCS, p₅, Fin.isValue, p₆, List.mem_cons, List.not_mem_nil, or_false] at hp
   rcases hp with hp | hp
   · rw [hp]
-    sorry
-  sorry
+    decide +kernel
+  rw [hp]
+  decide +kernel
 lemma lCS_pairwise : lCS.Pairwise fun p q ↦ p.vars.max < q.vars.max := sorry
 
 def CS : TriangularSet (Fin 8) ℚ := TriangularSet.list lCS lCS_non_zero lCS_pairwise
@@ -40,9 +43,12 @@ theorem hCS : CS.isCharacteristicSet ℚ l := by
     · use [99, 99]
       use [99, 99]
       simp [CS, TriangularSet.length_list, TriangularSet.list_apply, lCS]
+
       sorry
-    · sorry
-    · sorry
+    ·
+      sorry
+    ·
+      sorry
     sorry
     ----------
   rw [vanishingSet_eq_zeroLocus_span', vanishingSet_eq_zeroLocus_span']
@@ -71,7 +77,9 @@ example (h₁ : p₁ = 0) (h₂ : p₂ = 0) (h₃ : p₃ = 0) (h₄ : p₄ = 0) 
     simp only [vanishingSet, aeval_eq_eval, List.mem_cons, List.not_mem_nil, or_false, forall_eq,
       Set.setOf_subset_setOf]
     intro x hx
-    have h : I.eval x ≠ 0 := sorry
+    have h : I.eval x ≠ 0 := by
+      
+      sorry
     have hx : ∀ p ∈ CS, (eval x) p = 0 := hCS.2 hx
     have : (I * p₅).eval x = 0 := by
       simp only [heq, Fin.getElem_fin, map_sum, map_mul]
