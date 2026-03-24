21c21426630832be09e162213483465f0db7fca8
tsuki8 <3118181069@qq.com>
2026-03-24 14:55:12 +0800

    add miniexample

--- CharacteristicSet/MiniExample.lean
+++ CharacteristicSet/MiniExample.lean
@@ -13,14 +13,55 @@ def l : List ℚ[Fin 8] :=
 
 def lCS : List ℚ[Fin 8] := [X 0 + X 3 - (X 1 + X 2), X 4 + X 7 - (X 5 + X 6)]
 
-lemma lCS_non_zero : ∀ p ∈ lCS, p ≠ 0 := fun p hp ↦ by
-  simp only [lCS, Fin.isValue, List.mem_cons, List.not_mem_nil, or_false] at hp
+def computed_CS : List ℚ[Fin 8] := [X 0 + X 3 - (X 1 + X 2), X 4 + X 7 - (X 5 + X 6)]
+
+lemma CS_non_zero : ∀ p ∈ computed_CS, p ≠ 0 := fun p hp ↦ by
+  simp only [computed_CS, Fin.isValue, List.mem_cons, List.not_mem_nil, or_false] at hp
   rcases hp with hp | hp
   · rw [hp]
     decide +kernel
   rw [hp]
   decide +kernel
 
-lemma lCS_pairwise : lCS.Pairwise fun p q ↦ p.vars.max < q.vars.max := by
+
+lemma CS_pairwise : computed_CS.Pairwise fun p q ↦ p.vars.max < q.vars.max := by
   sorry
 
+def CS : TriangularSet (Fin 8) ℚ := TriangularSet.list computed_CS CS_non_zero CS_pairwise
+
+theorem hCS : CS.isCharacteristicSet ℚ l := by
+  constructor
+  · intro g hg
+    unfold isSetRemainder
+    constructor
+    · exact MvPolynomial.zero_reducedToSet
+      ----------
+    simp only [l, List.mem_cons, List.not_mem_nil, or_false] at hg
+    rcases hg with hg | hg | hg | hg
+    · use [1, 1], [1, 1]
+      simp
+      rw [hg]
+
+
+      sorry
+    ·
+      sorry
+    ·
+      sorry
+    sorry
+    ---------
+  rw [vanishingSet_eq_zeroLocus_span', vanishingSet_eq_zeroLocus_span']
+  apply zeroLocus_anti_mono
+  have : {p | p ∈ CS} = {p | p ∈ lCS} := by
+    ext p
+    simp only [SetLike.setOf_mem_eq, SetLike.mem_coe, Set.mem_setOf_eq, CS]
+    exact TriangularSet.mem_list_iff CS_non_zero CS_pairwise
+  rw [l, this, lCS]
+  simp only [List.mem_cons, List.not_mem_nil, or_false, ge_iff_le]
+  have heq1 (p q : ℚ[Fin 8]) : {r | r = p ∨ r = q} = {p, q} := Set.insert_def ..
+  have heq2 (p1 p2 p3 p4 : ℚ[Fin 8]) : {r | r = p1 ∨ r = p2 ∨ r = p3 ∨ r = p4} = {p1, p2, p3, p4} :=
+    Set.insert_def ..
+  rw [heq1, heq2]
+    ----------
+    ----------
+  sorry
